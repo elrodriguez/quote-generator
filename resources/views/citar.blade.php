476acd73f7ee7aa63ee7c900aec450be24dfdc9e
elrodriguez <elrodriguez2423@gmail.com>
2023-03-31 12:13:07 -0500

    elmer

--- resources/views/citar.blade.php
+++ resources/views/citar.blade.php
@@ -5,34 +5,45 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Bootstrap demo</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
-    
     </head>
     <body>
         <div class="container">
             <h1>Generador de citas</h1>
             <form id="form-citar" action="{{ route('citar') }}" method="POST">
-                <div class="mb-3">
-                    <label for="url">DOI del artículo:</label>
-                    <input class="form-control" type="text" id="url" name="url" required value="10.17993/ingytec.2018.45">
+                <div class="row mb-4 g-3 align-items-center">
+                    <div class="col-3">
+                      <label for="url" class="col-form-label text-right">DOI del artículo:</label>
+                    </div>
+                    <div class="col-9">
+                        <input class="form-control" type="text" id="url" name="url" required value="10.17993/ingytec.2018.45">
+                    </div>
                 </div>
-                <div class="mb-3">
-                    <label for="normativa">Normativa de citación:</label>
-                    <select class="form-select" id="normativa" name="normativa" required>
-                        <option value="apa">APA</option>
-                        <option value="mla">MLA</option>
-                        <option value="harvard">Harvard</option>
-                        <option value="iso690">ISO690</option>
-                        <option value="ieee">IEEE</option>
-                        <option value="chicago">Chicago</option>
-                        <option value="vancouver">Vancouver</option>
-                    </select>
+                <div class="row mb-4 g-3 align-items-center">
+                    <div class="col-3">
+                      <label for="normativa" class="col-form-label text-right">Normativa de citación:</label>
+                    </div>
+                    <div class="col-9">
+                        <select class="form-select" id="normativa" name="normativa" required>
+                            <option value="apa">APA</option>
+                            <option value="mla">MLA</option>
+                            <option value="harvard">Harvard</option>
+                            <option value="iso690">ISO690</option>
+                            <option value="ieee">IEEE</option>
+                            <option value="chicago">Chicago</option>
+                            <option value="vancouver">Vancouver</option>
+                        </select>
+                    </div>
                 </div>
-                <input type="submit" class="btn btn-primary" value="Generar cita">
-              </form>
+                <div class="row">
+                    <div class="col-9 offset-md-3">
+                        <input type="submit" class="btn btn-primary" value="Generar cita">
+                    </div>
+                </div>
+            </form>
             <div class="mt-4" id="divRes" style="display:none">
                 <div class="card">
                     <div class="card-header d-flex justify-content-between align-items-center">
-                        <h5 class="m-0">Cita</h5>
+                        <h5 class="m-0">Resultado</h5>
                         <button class="btn btn-outline-secondary btn-sm ml-auto" id="copiar">
                             <i class="bi bi-clipboard"></i> Copiar
                           </button>
